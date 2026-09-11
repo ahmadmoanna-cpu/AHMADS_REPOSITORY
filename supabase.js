fbfc8586858fd8c2c7046345af2d155344bd682f
ahmadmoanna-cpu <ahmadmoanna@gmail.com>
2026-09-11 14:39:46 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ijxvizumhdqipqslrhe.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_M8TCejjrWptCnHTY4vPkfg_aTssAm5H";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
